b8ac49eabd532b38536cb9a9dfb5b7e9b004b19f
Sunhong Kim <shkim@sunhong-ui-imac.local>
2016-06-22 22:12:17 +0900

    Initial Commit

---
+++ HIUIScatterCardCardViewSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HIUIScatterCardCardViewSample
+//
+//  Created by Sunhong Kim on 2016. 6. 22..
+//  Copyright © 2016년 Sunhong Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
